4b00ef342bd217dfcf604b5261c80a7a1c25e554
amila <amila@zeptolytics.com>
2017-10-19 18:02:25 +0530

    update index.js

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ module.exports = test-api-key;
  * Create a `test-api-key`
  *
  * @param {String} key-length | default : 16
- * @param {String} key-prefiex
+ * @param {String} key-prefix
  * @return {String} test-api-key
  */
 
-function zuid(key-length,key-prefiex) {
+function zuid(key_length,key_prefix) {
     
-    if (isNaN(key-length)) {
-        throw new Error('error in lenght : value is NaN : \"' + len + '\"');
+    if (isNaN(key_length)) {
+        throw new Error('error in key-lenght : value is NaN : \"' + key_length + '\"');
     }
-    if(key-prefiex){
-        key-prefiex = key-prefiex + '_';
+    if(key_prefix){
+        key_prefix = key_prefix + '_';
     }else{
-        key-prefiex = '';
+        key_prefix = '';
     }
-    key-length = key-length || 16;
-    return key-prefiex + Math.random().toString(32).substr(2, len);;
+    key_length = key_length || 16;
+    return key_prefix + Math.random().toString(32).substr(2, len);;
 }
